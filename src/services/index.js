688a1b8f06146b0ef0107d125bfeb8bb93c812d2
johnrobison <robisonj8680@gmail.com>
2021-07-07 09:16:11 -0400

    added index.js and additional boilerplate

---
+++ src/services/index.js
@@ -0,0 +1,7 @@
+export const config = {
+    headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`
+    },
+};
+
+export const baseURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/healUversionTwo`;
