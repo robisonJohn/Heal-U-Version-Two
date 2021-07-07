b5a3740ada732e2dd6cf34c11c646f135f9913c7
johnrobison <robisonj8680@gmail.com>
2021-07-07 09:41:54 -0400

    imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import logo from './logo.svg';
+// MAIN IMPORTS
 import './App.css';
+import axios from 'axios';
+
+// COMPONENT IMPORTS
+import Navbar from './components/Navbar/Navbar.jsx';
+import Header from './components/Header/Header.jsx';
+import ParticlesWrapper from "./components/Particles/ParticlesWrapper.jsx";
+import Feeling from "./components/Feeling/Feeling.jsx";
+import About from "./components/About/About.jsx";
+import Form from "./components/Form/Form.jsx";
+import BarChart from './components/BarChart/BarChart.jsx';
+// import BarChart from './components/BarChart.jsx';
+
+import { Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { baseURL, config } from "./services";
+
+import { Table } from 'react-bootstrap';
 
 function App() {
+  const [feelings, setFeelings] = useState([]);
+  const [toggleFetch, setToggleFetch] = useState(true);
+
+  useEffect(() => {
+    const retrieveFeelings = async () => {
+      const resp = await axios.get(baseURL, config);
+      setFeelings(resp.data.records);
+      console.log(resp.data.records);
+      
+    }
+    retrieveFeelings();
+    
+  }, [toggleFetch]);
+
+  console.log(feelings);
   return (
-    <div className="App">
-      <h1>hello there</h1>
-    </div>
+    
+    <h1>hello there</h1>
   );
 }
 
